20af64efb7754ad2c8fa61a3a2116cd9acccc6b3
TheCoinHustlers <158062420+TheCoinHustlers@users.noreply.github.com>
2024-01-29 17:11:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example of a basic JavaScript functionality
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM is fully loaded.');
+    // You can add more interactive JavaScript code here
+});
